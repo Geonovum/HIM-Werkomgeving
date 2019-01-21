e34c4f04b64f60ede419d495d30faab921d27258
Gelder <Jan.van.Gelder@alliander.com>
2019-01-21 15:59:13 +0100

    publishdate aangepast

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ var respecConfig =
   format: "markdown",                   // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2018-09-18",  	      // Format is "YYY-MM-DD"
+  publishDate: "2019-01-21",  	      // Format is "YYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
